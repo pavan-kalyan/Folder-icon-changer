34d917a040a96e508d1fb1ea7814099b30094820
pavan-kalyan <damalapati.pavan@gmail.com>
2017-08-25 23:11:31 +0530

    Add files via upload
    -it now loops through all folder names in the working directory.

--- iconChanger.py
+++ iconChanger.py
@@ -2,35 +2,31 @@
 import requests
 import urllib
 import bs4 
-from os.path  import basename
-#var=input('Please enter the name of the T.V show ')
-var = "How I Met Your Mother"
+import os
+from os.path import basename
+
+print(os.getcwd())
+
+urlBase="https://fanart.tv"
 url1="https://fanart.tv/tv-fanart/"
-url1=url1 +var[0]
-url2="https://fanart.tv"
-r=requests.get(url1)
-soup=bs4.BeautifulSoup(r.text)
-#print(soup.prettify())
-for link in soup.find_all(text =var, href=True):
-    url2=url2+link['href']
-    print(url2)
-r2=requests.get(url2)
-soup2=bs4.BeautifulSoup(r2.content)
+for fileName in os.listdir(os.getcwd()):
+	#print(fileName)
+	if os.path.isdir(fileName):
 
-elem=soup2.select('img')
-i=0
-for title in elem:
-	if title.get('title')==var+" tv poster image":
-		link=title.get('src')
-		i=i+1
-		if(i==1):
+		url1=urlBase+"/tv-fanart/"+fileName[0]
+		r=requests.get(url1)
+		soup=bs4.BeautifulSoup(r.text,"lxml")
+		for link in soup.find_all(text =fileName, href=True):
+			url2=urlBase+link['href']
+			print(url2)
 			break
-#for link in soup2.find_all('bigger_image', href=True):
-#   print(link['href'])
-	#print(link)
-file_name = var
-url2="https://fanart.tv"
-print(url2+link)
-urllib.request.urlretrieve(url2+link, file_name)
-
+		r=requests.get(url2)
+		soup=bs4.BeautifulSoup(r.content,"lxml")
+		images=soup.select('img')
+		for title in images:
+			if title.get('title')==fileName+" tv poster image":
+				link=title.get('src')
+				print(urlBase+link)
+				break
+		urllib.request.urlretrieve(urlBase+link,os.path.join(os.path.join(os.getcwd(),fileName),fileName) )
 
