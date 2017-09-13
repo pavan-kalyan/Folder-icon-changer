b639d2885871696935c48c335bd87309669c1d2a
pavan-kalyan <damalapati.pavan@gmail.com>
2017-09-13 14:44:52 +0530

    Add files via upload

--- iconChanger.py
+++ iconChanger.py
@@ -5,38 +5,42 @@ import bs4
 import os
 from os.path import basename
 
-print(os.getcwd())
-
 urlBase="https://fanart.tv"
 url1="https://fanart.tv/tv-fanart/"
 #loops through all folder names in current directory and downloads image.
 for fileName in os.listdir(os.getcwd()):
-	
-	if os.path.isdir(fileName):
 
-		url1=urlBase+"/tv-fanart/"+fileName[0]
-		r=requests.get(url1)
-		soup=bs4.BeautifulSoup(r.text,"lxml")
-		for link in soup.find_all(text =fileName, href=True):
-			url2=urlBase+link['href']
-			print(url2)
-			break
-		r=requests.get(url2)
-		soup=bs4.BeautifulSoup(r.content,"lxml")
-		images=soup.select('img')
-		for title in images:
-			if title.get('title')==fileName+" tv poster image":
-				link=title.get('src')
-				print(urlBase+link)
-				if link:
-					break
-				else:
-					continue
+    if os.path.isdir(fileName):
+        
+        url1=urlBase+"/tv-fanart/"+fileName[0]
+        r=requests.get(url1)
+        soup=bs4.BeautifulSoup(r.text,"lxml")
 
-		
-		if(not isinstance(link, str)):
-			continue
-		urllib.request.urlretrieve(urlBase+link,os.path.join(os.path.join(os.getcwd(),fileName),fileName))
-		
-		os.system("gvfs-set-attribute -t string \""+ os.path.join(os.getcwd(),fileName) +"\" metadata::custom-icon file://\""+os.path.join(os.path.join(os.getcwd(),fileName),fileName)+"\"")
+        for link in soup.find_all(text =fileName, href=True):
+            url2=urlBase+link['href']
+            print(url2)
+            r2=requests.get(url2)
+            soup2=bs4.BeautifulSoup(r2.content,"lxml")
+            images=soup2.select('img')
 
+            for title in images:
+                if(not isinstance(title.get('title'), str)):
+                    continue
+                if " tv poster image" in title.get('title'):
+                    link=title.get('src')
+                    print(urlBase+link)
+                    break
+
+                
+
+            #checks if image exists on webpage,if not ignore the folder.
+            if(not isinstance(link, str)):
+                continue
+            else:
+                break
+        if(not isinstance(link, str)):
+            continue
+        print('downloaded')
+        urllib.request.urlretrieve(urlBase+link,os.path.join(os.path.join(os.getcwd(),fileName),fileName))
+        #opens up system shell to change icons
+        os.system("gvfs-set-attribute -t string \""+ os.path.join(os.getcwd(),fileName) +"\" metadata::custom-icon file://\""+os.path.join(os.path.join(os.getcwd(),fileName),fileName)+"\"")
